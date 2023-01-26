0013de6eba13108d1d885d9a04104d0288162177
jayzhen <jayzhen_testing@163.com>
2023-01-26 19:45:46 +0800

    first commit

---
+++ config/defaultSettings.ts
@@ -0,0 +1,23 @@
+import { Settings as LayoutSettings } from '@ant-design/pro-layout';
+
+const Settings: LayoutSettings & {
+  pwa?: boolean;
+  logo?: string;
+} = {
+  navTheme: 'light',
+  // 拂晓蓝
+  primaryColor: '#1890ff',
+  layout: 'mix',
+  contentWidth: 'Fluid',
+  fixedHeader: false,
+  fixSiderbar: true,
+  colorWeak: false,
+  title: 'UTS',
+  pwa: false,
+  logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
+  iconfontUrl: '',
+  headerHeight: 48,
+  footerRender: false,
+};
+
+export default Settings;
